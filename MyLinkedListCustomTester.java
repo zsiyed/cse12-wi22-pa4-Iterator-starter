3d9c6404d4295019cd65f5b261d2740d73be3b9e
Andrew Tang <andrewtang9@gmail.com>
2022-01-24 09:48:27 -0800

    Add starter code

---
+++ MyLinkedListCustomTester.java
@@ -0,0 +1,115 @@
+
+/**
+ * TODO: Add your file header
+ * Name:
+ * ID:
+ * Email:
+ * Sources used: Put "None" if you did not have any external help
+ * Some example of sources used would be Tutors, Zybooks, and Lecture Slides
+ * 
+ * 2-4 sentence file description here
+ */
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * TODO: Add your class header
+ * 
+ * IMPORTANT: Do not change the method headers and points are awarded
+ * only if your test cases cover cases that the public tester file
+ * does not take into account.
+ */
+public class MyLinkedListCustomTester {
+
+    /**
+     * This sets up the test fixture. JUnit invokes this method before
+     * every testXXX method. The @Before tag tells JUnit to run this method
+     * before each test.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    /**
+     * TODO: test the hasNext method when [fill in a possible edge case here]
+     */
+    @Test
+    public void testHasNext() {
+
+    }
+
+    /**
+     * TODO: test the next method when [...]
+     */
+    @Test
+    public void testNext() {
+
+    }
+
+    /**
+     * TODO: test the hasPrevious method when [fill in another one here]
+     */
+    @Test
+    public void testHasPrevious() {
+
+    }
+
+    /**
+     * TODO: test the previous method when [...]
+     */
+    @Test
+    public void testPrevious() {
+
+    }
+
+    /**
+     * TODO: test the nextIndex method when [...]
+     */
+    @Test
+    public void testNextIndex() {
+
+    }
+
+    /**
+     * TODO: test the previousIndex method when [...]
+     */
+    @Test
+    public void testPreviousIndex() {
+
+    }
+
+    /**
+     * TODO: test the set method when [...]
+     */
+    @Test
+    public void testSet() {
+
+    }
+
+    /**
+     * TODO: test the remove method when [...]
+     */
+    @Test
+    public void testRemoveTestOne() {
+
+    }
+
+    /**
+     * TODO: test the remove method when [fill in another one here]
+     */
+    @Test
+    public void testRemoveTestTwo() {
+
+    }
+
+    /**
+     * TODO: test the add method when [...]
+     */
+    @Test
+    public void testAdd() {
+
+    }
+
+}
---
+++ MyLinkedListPublicTester.java
@@ -0,0 +1,256 @@
+/**
+ * This file contains all the public tests (visible on Gradescope)
+ * Use this as a guide to write tests to verify your MyLinkedList and Iterator 
+ * implementation 
+ * Iterator values were set manually to decouple its dependency on next() 
+ * during testing.
+ */
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class MyLinkedListPublicTester {
+    private MyLinkedList listLen1, listLen2;
+    private MyLinkedList.MyListIterator listLen1Iter, listLen2Iter;
+
+    @Before
+    public void setUp() {
+        listLen1 = new MyLinkedList();
+        listLen1.add("Christine");
+        listLen1Iter = listLen1.new MyListIterator();
+
+        listLen2 = new MyLinkedList();
+        listLen2.add("Paul");
+        listLen2.add("Cao");
+        listLen2Iter = listLen2.new MyListIterator();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertTrue("The initial value of forward", listLen1Iter.forward);
+        assertFalse("The initial value of canRemoveOrSet", listLen1Iter.canRemoveOrSet);
+        assertEquals("The index of the element the iterator is initially pointing on", 0, listLen1Iter.idx);
+        assertEquals("The data of initial iterator's left", listLen1.head, listLen1Iter.left);
+        assertEquals("The data of initial iterator's left", null, listLen1Iter.left.data);
+        assertEquals("The data of initial iterator's right", "Christine", listLen1Iter.right.data);
+    }
+
+    @Test
+    public void testHasNext() {
+        assertTrue("call hasNext when there is a next node", listLen2Iter.hasNext());
+        // Manually doing listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.canRemoveOrSet = true;
+        listLen2Iter.forward = true;
+        assertTrue("call hasNext when there is a next node", listLen2Iter.hasNext());
+        // Manually doing listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext().getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext().getNext();
+        listLen2Iter.idx = 2;
+        listLen2Iter.canRemoveOrSet = true;
+        listLen2Iter.forward = true;
+        assertFalse("call hasNext when there isn't a next node", listLen2Iter.hasNext());
+    }
+
+    @Test
+    public void testNextOneCall() {
+        assertEquals("The element returned from calling next", "Christine", listLen1Iter.next());
+        assertEquals("Index of iterator after 1 next()", 1, listLen1Iter.idx);
+        assertTrue("Able to remove node", listLen1Iter.canRemoveOrSet);
+        assertTrue("Direction is forward", listLen1Iter.forward);
+    }
+
+    @Test
+    public void testNextTwoCalls() {
+        assertEquals("The element returned from calling next", "Paul", listLen2Iter.next());
+        assertEquals("Index of iterator after 1 next()", 1, listLen2Iter.idx);
+        assertEquals("The element returned from calling next", "Cao", listLen2Iter.next());
+        assertEquals("Index of iterator after 2 next()", 2, listLen2Iter.idx);
+        assertTrue("Able to remove node", listLen2Iter.canRemoveOrSet);
+        assertTrue("Direction is forward", listLen2Iter.forward);
+}
+
+    @Test
+    public void testHasPrev() {
+        assertFalse("call when there isn't a previous node", listLen2Iter.hasPrevious());
+        // Manually doing listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.canRemoveOrSet = true;
+        listLen2Iter.forward = true;
+        assertTrue("call when there is a previous node", listLen2Iter.hasPrevious());
+        // Manually doing listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext().getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext().getNext();
+        listLen2Iter.idx = 2;
+        listLen2Iter.canRemoveOrSet = true;
+        listLen2Iter.forward = true;
+        assertTrue("call when there is a previous node", listLen2Iter.hasPrevious());
+    }
+
+    @Test
+    public void testPreviousOneCall() {
+        // Manually doing listLen1Iter.next();
+        listLen1Iter.left = listLen1.head.getNext();
+        listLen1Iter.right = listLen1.head.getNext().getNext();
+        listLen1Iter.idx = 1;
+        listLen1Iter.canRemoveOrSet = true;
+        listLen1Iter.forward = true;
+        assertEquals("The element returned from calling previous", "Christine", listLen1Iter.previous());
+        assertEquals("Index of iterator after 0 previous()", 0, listLen1Iter.idx);
+        assertTrue("Able to remove node", listLen1Iter.canRemoveOrSet);
+        assertFalse("Direction is not forward", listLen1Iter.forward);
+    }
+
+    @Test
+    public void testPreviousNoPrevious() {
+        // Manually doing listLen1Iter.next();
+        listLen1Iter.left = listLen1.head.getNext();
+        listLen1Iter.right = listLen1.head.getNext().getNext();
+        listLen1Iter.idx = 1;
+        listLen1Iter.canRemoveOrSet = true;
+        listLen1Iter.forward = true;
+        assertEquals("The element returned from calling previous", "Christine", listLen1Iter.previous());
+        assertEquals("Index of iterator after 2 previous()", 0, listLen1Iter.idx);
+        assertTrue("Able to remove node", listLen1Iter.canRemoveOrSet);
+        assertFalse("Direction is not forward", listLen1Iter.forward);
+        try {
+            listLen1Iter.previous();    // Should throw NoSuchElementException
+            fail();
+        } catch (NoSuchElementException e) {
+            // do nothing
+        }
+    }
+
+    @Test
+    public void testNextIndex() {
+        assertEquals("Index before any moves", 0, listLen2Iter.nextIndex());
+        // Manually doing listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        assertEquals("Index after 1 next", 1, listLen2Iter.nextIndex());
+    }
+
+    @Test
+    public void testNextIndexLastNode() {
+        listLen2Iter.left = listLen2.head.getNext().getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext().getNext();
+        listLen2Iter.idx = 2;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        assertEquals("Index after 2 next", 2, listLen2Iter.nextIndex());
+    }
+
+    @Test
+    public void testPrevIndex() {
+        listLen2Iter.left = listLen2.head.getNext().getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext().getNext();
+        listLen2Iter.idx = 2;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        assertEquals("Index after 2 next, no previous", 1, listLen2Iter.previousIndex());
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        assertEquals("Index after 2 next, 1 previous", 0, listLen2Iter.previousIndex());
+    }
+
+    @Test
+    public void testPrevIndexFirstNode() {
+        assertEquals("Previous index at first node", -1, listLen2Iter.previousIndex());
+    }
+
+    @Test
+    public void testSetForward() {
+         // equivalent to listLen2Iter.next();
+         listLen2Iter.left = listLen2.head.getNext();
+         listLen2Iter.right = listLen2.head.getNext().getNext();
+         listLen2Iter.idx = 1;
+         listLen2Iter.forward = true;
+         listLen2Iter.canRemoveOrSet = true;
+         
+         listLen2Iter.set("Did it change?");
+         assertEquals("Valid set when forward", "Did it change?", listLen2Iter.left.getElement());
+         assertEquals("Valid set when forward, shouldn't change", "Cao", listLen2Iter.right.getElement());
+    }
+    
+    @Test
+    public void testSetBackward() {
+        // equivalent to 2 next() and 1 previous()
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = false;
+        listLen2Iter.canRemoveOrSet = true;
+        
+        listLen2Iter.set("Did it change?");
+        assertEquals("Valid set when backward", "Did it change?", listLen2Iter.right.getElement());
+        assertEquals("Valid set when backward, shouldn't change", "Paul", listLen2Iter.left.getElement());
+    }
+
+    @Test
+    public void testRemoveForward() {
+        // equivalent to listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        
+        listLen2Iter.remove();
+        assertEquals("Valid remove when forward", null, listLen2Iter.left.getElement());
+        assertEquals("Valid remove when forward, shouldn't change", "Cao", listLen2Iter.right.getElement());
+        assertEquals("Index should decrement after removal", 0, listLen2Iter.idx);
+        assertFalse("Prevent another remove or set", listLen2Iter.canRemoveOrSet);
+    }
+
+    @Test
+    public void testRemoveBackward() {
+        // equivalent to listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = false;
+        listLen2Iter.canRemoveOrSet = true;
+        
+        listLen2Iter.remove();
+        assertEquals("Valid remove when forward", null, listLen2Iter.right.getElement());
+        assertEquals("Valid remove when backward, shouldn't change", "Paul", listLen2Iter.left.getElement());
+        assertEquals("Index shouldn't decrement after removal", 1, listLen2Iter.idx);
+        assertFalse("Prevent another remove or set", listLen2Iter.canRemoveOrSet);
+    }
+
+    @Test
+    public void testAddValidIndex() {
+        // equivalent to listLen2Iter.next();
+        listLen2Iter.left = listLen2.head.getNext();
+        listLen2Iter.right = listLen2.head.getNext().getNext();
+        listLen2Iter.idx = 1;
+        listLen2Iter.forward = true;
+        listLen2Iter.canRemoveOrSet = true;
+        
+        listLen2Iter.add("CSE 12");
+        assertEquals("Valid add left", "CSE 12", listLen2Iter.left.getElement());
+        assertEquals("Valid add right", "Cao", listLen2Iter.right.getElement());
+        assertEquals("Index change after add", 2, listLen2Iter.idx);
+        assertFalse("Cannot remove immediately after add", listLen2Iter.canRemoveOrSet);
+    }
+
+    @Test
+    public void testAddFirstIndex() {
+        listLen2Iter.add("CSE 12");
+        assertEquals("Valid add left", "CSE 12", listLen2Iter.left.getElement());
+        assertEquals("Valid add right", "Paul", listLen2Iter.right.getElement());
+        assertEquals("Index change after add", 1, listLen2Iter.idx);
+        assertFalse("Cannot remove immediately after add", listLen2Iter.canRemoveOrSet);
+    }
+}
