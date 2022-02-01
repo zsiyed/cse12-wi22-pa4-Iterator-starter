c20e4e49eddf444d7fd840078855d6ab18c55f60
rohunkulshrestha <rohunk03@gmail.com>
2022-02-01 01:11:45 -0600

    Custom Tests

--- MyLinkedListCustomTester.java
+++ MyLinkedListCustomTester.java
@@ -12,6 +12,7 @@
 
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 /**
  * TODO: Add your class header
@@ -21,6 +22,8 @@ import org.junit.*;
  * does not take into account.
  */
 public class MyLinkedListCustomTester {
+    private MyLinkedList testList, testEmptyList;
+    private MyLinkedList.MyListIterator testListIter, testEmptyListIter;
 
     /**
      * This sets up the test fixture. JUnit invokes this method before
@@ -29,14 +32,24 @@ public class MyLinkedListCustomTester {
      */
     @Before
     public void setUp() throws Exception {
+        testList = new MyLinkedList();
+        testList.add("A");
+        testList.add("B");
+        testList.add("C");
+        testListIter = testList.new MyListIterator();
 
+        testEmptyList = new MyLinkedList();
+        testEmptyListIter = testEmptyList.new MyListIterator();
     }
 
     /**
-     * TODO: test the hasNext method when [fill in a possible edge case here]
+     * test the hasNext method when idx is at it's max value
      */
     @Test
     public void testHasNext() {
+        testListIter.idx = testList.size;
+
+        assertEquals("hasNext should return false", false, testListIter.hasNext());
 
     }
 
@@ -45,6 +58,16 @@ public class MyLinkedListCustomTester {
      */
     @Test
     public void testNext() {
+        testListIter.idx = testList.size;
+
+        boolean exceptionThrown = false;
+        try {
+            testListIter.next();
+        } catch (NoSuchElementException e) {
+            exceptionThrown = true;
+            System.out.println("Succesfully Caught NoSuchElementException @ testNext()");
+        }
+        assertTrue("testNext()", exceptionThrown);
 
     }
 
@@ -53,47 +76,84 @@ public class MyLinkedListCustomTester {
      */
     @Test
     public void testHasPrevious() {
-
     }
 
     /**
-     * TODO: test the previous method when [...]
+     * TODO: test to make sure a NoSuchElementExceotion is thrown when applicable
      */
     @Test
     public void testPrevious() {
+        testListIter.idx = 0;
+
+        boolean exceptionThrown = false;
+        try {
+            testListIter.previous();
+        } catch (NoSuchElementException e) {
+            exceptionThrown = true;
+            System.out.println("Succesfully Caught NoSuchElementException @ testNext()");
+        }
+        assertTrue("testHasPrevious()", exceptionThrown);
 
     }
 
     /**
-     * TODO: test the nextIndex method when [...]
+     * TODO: test the nextIndex method with an empty list
      */
     @Test
     public void testNextIndex() {
-
+       assertEquals("Checks nextIndex with an empty list", 0, testEmptyListIter.nextIndex());
     }
 
     /**
-     * TODO: test the previousIndex method when [...]
+     * TODO: test the previousIndex method with an empty list
      */
     @Test
     public void testPreviousIndex() {
-
+        assertEquals("Checks nextIndex with an empty list", -1, testEmptyListIter.previousIndex());
     }
 
     /**
-     * TODO: test the set method when [...]
+     * TODO: test the set method's ability to catch applicable exceptions
      */
     @Test
     public void testSet() {
+        boolean exceptionThrownOne = false;
+        boolean exceptionThrownTwo = false;
+        try {
+            testListIter.set(null);
+        } catch (NullPointerException e) {
+            exceptionThrownOne = true;
+            System.out.println("Succesfully Caught NullPointerException @ testSet()");
+        }
 
+        testListIter.next();
+        testListIter.add("E");
+        try {
+            testListIter.set("D");
+        } catch (IllegalStateException e) {
+            exceptionThrownTwo = true;
+            System.out.println("Succesfully Caught IllegalStateException @ testSet()");
+        }
+        assertTrue("testSet()", exceptionThrownOne);
+        assertTrue("testSet2()", exceptionThrownTwo);
     }
 
     /**
-     * TODO: test the remove method when [...]
+     * TODO: test the remove method when 
      */
     @Test
     public void testRemoveTestOne() {
+        boolean exceptionThrown = false;
 
+        testListIter.next();
+        testListIter.add("E");
+        try {
+            testListIter.remove();
+        } catch (IllegalStateException e) {
+            exceptionThrown = true;
+            System.out.println("Succesfully Caught IllegalStateException @ testRemove()");
+        }
+        assertTrue("testRemove()", exceptionThrown);
     }
 
     /**
@@ -102,6 +162,7 @@ public class MyLinkedListCustomTester {
     @Test
     public void testRemoveTestTwo() {
 
+        
     }
 
     /**
@@ -109,7 +170,11 @@ public class MyLinkedListCustomTester {
      */
     @Test
     public void testAdd() {
+        testListIter.idx = testList.size;
+        testListIter.add("F");
 
+        assertEquals("Check if element was added at the end of the list", "F", testListIter.left.getElement());
+        
     }
 
 }
